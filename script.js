0095c30334de83cf01bee31296d46b836234f39c
Saravanan Sellathurai <saravanan@L-156088098.local>
2017-03-20 11:39:32 +0000

    updated the javascript messages

--- script.js
+++ script.js
@@ -30,13 +30,13 @@
     // create the controller and inject Angular's $scope
     scotchApp.controller('mainController', function($scope) {
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
+        $scope.message = 'Welcome to my home page';
     });
 
     scotchApp.controller('aboutController', function($scope) {
-        $scope.message = 'Look! I am an about page.';
+        $scope.message = 'About page';
     });
 
     scotchApp.controller('contactController', function($scope) {
-        $scope.message = 'Contact us! JK. This is just a demo.';
+        $scope.message = 'contact us page';
     });
